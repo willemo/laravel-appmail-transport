f580d80cac8c4f25520a5604b76dce77e315ed8e
Willem Oostendorp <willem@soomedia.nl>
2017-02-14 17:45:46 +0100

    Created the service provider and config file

---
+++ config/laravel-appmail.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+     |-------------------------------------------------------------------------
+     | AppMail API Host
+     |-------------------------------------------------------------------------
+     |
+     | The AppMail host for sending API requests, defaults to "api.appmail.io"
+     */
+
+    'api_host' => env('APPMAIL_HOST', 'api.appmail.io'),
+
+    /*
+     |-------------------------------------------------------------------------
+     | AppMail API version
+     |-------------------------------------------------------------------------
+     |
+     | The AppMail API version, defaults to "v1"
+     */
+
+    'api_version' => env('APPMAIL_VERSION', 'v1'),
+
+    /*
+     |-------------------------------------------------------------------------
+     | AppMail API Key
+     |-------------------------------------------------------------------------
+     |
+     | You can generate the API key on the Credentials page for your server
+     */
+
+    'api_key' => env('APPMAIL_KEY'),
+
+];
---
+++ src/AppMailServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Willemo\LaravelAppMail;
+
+use Willemo\LaravelAppMail\AppMailTransport;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use AppMail\Client;
+
+class AppMailServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        app('swift.transport')->extend('appmail', function ($app) {
+            $apiKey = $app['config']->get('laravel-appmail.api_key');
+
+            return new AppMailTransport(new Client($apiKey));
+        });
+    }
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+
+        $configPath = __DIR__ . '/../config/laravel-appmail.php';
+
+        $this->publishes([$configPath => config_path('laravel-appmail.php')]);
+
+        $this->mergeConfigFrom($configPath, 'laravel-appmail');
+    }
+}
